2194eb147cc8e1e608a9707936ea998cc17b1f7d
Jeremy Toler <waddledoocanto@gmail.com>
2016-05-11 08:42:15 -0700

    Initial config with Visual Studio default MVC files.

---
+++ GameVote/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GameVote
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ GameVote/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameVote.MvcApplication" Language="C#" %>
